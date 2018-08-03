92da60b54ce78b9a86c7a5b6850951c649094a68
honghaifeng <18500893514@163.com>
2018-08-03 14:14:45 +0800

    fix

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var localStorages = {
+    install: function(Vue) {
+        if (!Vue.prototype.$localStorage) {
+            Object.defineProperties(Vue.prototype, {
+                $localStorage: {
+                    get: function() {
+                        return localStorages;
+                    },
+                },
+            });
+        }
+    },
     get: function (key) {
         return JSON.parse(localStorage.getItem(key));
     },
